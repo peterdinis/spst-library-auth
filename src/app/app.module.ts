f93df6fcb39f81ad8c54c08e5c17bae43c5f64aa
Peter Dinis <peterdinis0@pobox.sk>
2024-04-28 08:46:58 +0200

    infra: Formatting files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,11 @@
-import { Module } from "@nestjs/common";
-import { PrismaModule } from "src/prisma/prisma.module";
-import { AuthService } from "./auth.service";
-import { AuthController } from "./auth.controller";
+import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-    imports: [PrismaModule],
-    providers: [AuthService],
-    controllers: [AuthController]
+  imports: [PrismaModule],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
-
 export class AuthModule {}
