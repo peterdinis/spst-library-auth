5aacfd19aa68298782c5ec6b73c7a576389506e7
Peter Dinis <peterdinis0@pobox.sk>
2024-04-28 10:06:32 +0200

    feat: Updating dtos

--- src/auth/dto/create-user-dto.ts
+++ src/auth/dto/create-user-dto.ts
@@ -1,19 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   lastName: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   password: string;
--- src/auth/dto/view-user-dto.ts
+++ src/auth/dto/view-user-dto.ts
@@ -1,3 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class ViewUserDto {
-    
+   @ApiProperty()
+   id: string;
+
+   @ApiProperty()
+   name: string;
+
+   @ApiProperty()
+   lastName: string;
+
+   @ApiProperty()
+   email: string;
+
+   @ApiProperty()
+   password: string;
+
+   @ApiProperty()
+   role: string;
+
+   @ApiProperty()
+   isActive: boolean;
+
+   @ApiProperty()
+   createdAt: Date;
+
+   @ApiProperty()
+   updatedAt: Date;
 }
