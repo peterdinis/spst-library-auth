cd59fc3edc0b4754bf46400e5e3e42ad33e580c5
Peter Dinis <peterdinis0@pobox.sk>
2024-04-28 11:59:07 +0200

    FOO

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
-
+@ApiTags("Example")
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({
+    description: "Testing endpoint for our auth server"
+  })
+  @ApiOkResponse()
   @Get()
   getHello(): string {
     return this.appService.getHello();
