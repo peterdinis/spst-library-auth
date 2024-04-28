fddc7422af7f5ab1a6d927513ce9951099304c1b
Peter Dinis <peterdinis0@pobox.sk>
2024-04-28 12:16:49 +0200

    feat: Start updating auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,32 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { ViewUserDto } from './dto/view-user-dto';
 
 @ApiTags("Auth endpoints")
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @ApiOperation({
+        summary: "Return all users"
+    })
+    @ApiOkResponse({
+        type: [ViewUserDto]
+    })
+    @Get("/users")
+    async allUsers() {
+        return this.authService.getAllUsers();
+    }
+
+    @ApiOperation({
+        summary: "Find one user by id"
+    })
+    @ApiOkResponse({
+        type: ViewUserDto
+    })
+    @Get("/users/:id")
+    async getOneUser(@Param("id") id: string) {
+        return this.authService.findOneUser(id);
+    }
+}
