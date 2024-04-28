33d718878d895ae9e7d637f40bde27c89cb70648
Peter Dinis <peterdinis0@pobox.sk>
2024-04-28 10:19:45 +0200

    feat: Adding nestjs jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [AuthService],
+  imports: [PrismaModule, JwtModule.register({
+    global: true,
+    secret: process.env.JWT_SECRET as unknown as string,
+    signOptions: { expiresIn: '86 400s' },
+  }),],
+  providers: [AuthService, JwtService],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,30 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user-dto';
+import { hash, compare } from 'bcrypt';
+import { LoginDto } from './dto/login-user-dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly prismaService: PrismaService,
+        private readonly jwtService: JwtService
     ) {}
+
+    async validateUser(loginDto: LoginDto) {
+        const user = await this.prismaService.user.findFirst({
+            where: {
+                email: loginDto.email
+            }
+        })
+
+        if (user && (await compare(loginDto.password, user.password))) {
+            const { password, ...result } = user;
+            return result;
+        } else {
+            throw new UnauthorizedException("Zlé prihlasovacie údaje");
+        }
+    }
 
     async getAllUsers() {
         const allUsers = await this.prismaService.user.findMany();
@@ -53,5 +73,33 @@ export class AuthService {
         }
 
         return allTeachers;
+    }
+
+    async createNewUser(registerDto: CreateUserDto) {
+        const newUser = await this.prismaService.user.findFirst({
+            where: {
+                email: registerDto.email
+            }
+        })
+
+        if(newUser) {
+            throw new ConflictException("Používateľ s týmto emailom existuje");
+        }
+
+        const addNewUser = await this.prismaService.user.create({
+            data: {
+                ...registerDto,
+                isActive: true,
+                password: hash(registerDto.password, 10)
+            }
+        });
+
+        if(!addNewUser) {
+            throw new BadRequestException("Nastala chyba pri registrácií");
+        }
+
+        const {...result} = addNewUser;
+
+        return result;
     }
 }
