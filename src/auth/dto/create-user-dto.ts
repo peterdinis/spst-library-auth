57a108e9b877cb90b61a90a9f05027bb29d79519
Peter Dinis <peterdinis0@pobox.sk>
2024-04-28 09:57:55 +0200

    feat: Updating dtos

---
+++ src/auth/dto/create-user-dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
