d524eea4916d61a29e603722c5536d3892de2d87
Peter Dinis <peterdinis0@pobox.sk>
2024-05-11 13:06:58 +0200

    feat: Small updates

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,21 +1,46 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 interface CustomWebSocketClient extends Socket {}
 
 @WebSocketGateway()
 export class MessagesGateway {
-  @WebSocketServer() server: Server;
+    @WebSocketServer() server: Server;
 
-  @SubscribeMessage('adminRightsMessage')
-  handleAdminRightsMessage(client: CustomWebSocketClient, _: unknown): void {
-    const clientId = client.id;
-    this.server.to(clientId).emit('adminRightsMessage', 'Váš účet má prístupové práva');
-  }
+    @SubscribeMessage('adminRightsMessage')
+    handleAdminRightsMessage(client: CustomWebSocketClient, _: unknown): void {
+        const clientId = client.id;
+        this.server
+            .to(clientId)
+            .emit('adminRightsMessage', 'Váš účet má prístupové práva');
+    }
 
-  @SubscribeMessage('adminRemoveRights')
-  handleAdminRemoveRights(client: CustomWebSocketClient, _: unknown): void {
-    const clientId = client.id;
-    this.server.to(clientId).emit('adminRemoveRights', 'Vašmu účtu boli odobrané prístupové práva');
-  }
+    @SubscribeMessage('adminRemoveRights')
+    handleAdminRemoveRights(client: CustomWebSocketClient, _: unknown): void {
+        const clientId = client.id;
+        this.server
+            .to(clientId)
+            .emit(
+                'adminRemoveRights',
+                'Vašmu účtu boli odobrané prístupové práva',
+            );
+    }
+
+    @SubscribeMessage('deactivateAccount')
+    deactivateAccount(client: CustomWebSocketClient, _: unknown): void {
+        const clientId = client.id;
+        this.server
+            .to(clientId)
+            .emit('deactivateAccount', 'Váš účet bol deaktivovaný');
+    }
+
+    @SubscribeMessage('removeAccount')
+    removeAccount(client: CustomWebSocketClient, _: unknown): void {
+        const clientId = client.id;
+        this.server.to(clientId).emit('removeAccount', 'Váš účet bol zmazaný');
+    }
 }
