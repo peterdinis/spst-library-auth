6f4c8b5c5a812955e8910f506e7addde004eea1e
Peter Dinis <peterdinis0@pobox.sk>
2024-05-22 17:37:20 +0200

    FOO

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -1,26 +1,4 @@
-import { Injectable } from '@nestjs/common';
-import { CreateEmailDto } from './dto/create-email.dto';
-import { UpdateEmailDto } from './dto/update-email.dto';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class EmailsService {
-  create(createEmailDto: CreateEmailDto) {
-    return 'This action adds a new email';
-  }
-
-  findAll() {
-    return `This action returns all emails`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} email`;
-  }
-
-  update(id: number, updateEmailDto: UpdateEmailDto) {
-    return `This action updates a #${id} email`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} email`;
-  }
-}
+export class EmailsService {}
