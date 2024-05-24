27c1c750d8c54e5ee52a4bb513f891a7f98439af
Peter Dinis <peterdinis0@pobox.sk>
2024-05-24 20:55:49 +0200

    feat: Setup email endpoint

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -1,11 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { ResendService } from "nestjs-resend";
+import { SendEmailDto } from "./dto/send-email-dto";
 
 @Injectable()
 export class EmailsService {
   constructor(private readonly resendService: ResendService) {}
 
-  async sendEmail() {
-    
+  async sendEmail(sendEmailDto: SendEmailDto) {
+    const newEmail = await this.resendService.send({
+        from: sendEmailDto.from,
+        to: sendEmailDto.to,
+        subject: sendEmailDto.subject,
+        text: sendEmailDto.text
+    });
+
+    if(!newEmail) {
+      throw new ForbiddenException("Failed to send email");
+    }
+
+    return newEmail;
   }
 }
