de1d8b9f7f90e1a32d79cdeac0a2b1f6fb369957
Peter Dinis <peterdinis0@pobox.sk>
2024-05-27 13:03:41 +0200

    FOO

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
-    await app.listen(4000, '127.0.0.1');
+    await app.listen(4000);
 }
 bootstrap();
