9a133caaa1d566018841a3741cc1e8938499e3fd
Peter Dinis <peterdinis0@pobox.sk>
2024-06-02 21:34:28 +0200

    fix: Add hash option to user.model

--- src/auth/model/User.model.ts
+++ src/auth/model/User.model.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Schema({ collection: 'users', timestamps: true })
 export class User {
@@ -19,7 +20,7 @@ export class User {
     role: string;
 
     @Prop()
-    IsActive: boolean;
+    isActive: boolean;
 
     @Prop()
     hasAdminRights: boolean;
@@ -28,3 +29,23 @@ export class User {
 export type UserDocument = HydratedDocument<User>;
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+// Add pre-save middleware to hash the password
+UserSchema.pre('save', async function (next) {
+    const user = this as UserDocument;
+
+    // Only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    try {
+        // Generate a salt
+        const salt = await bcrypt.genSalt(10);
+        // Hash the password using the generated salt
+        user.password = await bcrypt.hash(user.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
